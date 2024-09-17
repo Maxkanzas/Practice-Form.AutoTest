0202b80e36f2a83175ae03b0d9d5b7ea3fc5d856
MaxxPC <maxkanz1991@gmail.com>
2024-09-17 20:38:05 +0400

    added comments

--- src/test/java/TestPracticeForm.java
+++ src/test/java/TestPracticeForm.java
@@ -3,6 +3,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selectors.byText;
@@ -15,6 +17,7 @@ public class TestPracticeForm {
                 Configuration.pageLoadStrategy = "eager"; // команда для того, чтобы селенид не ждал загрузки всех картинок и тяжелых элементов. Только html.
                 Configuration.browserSize = "1920x1080";
                 Configuration.baseUrl = "https://demoqa.com";
+                Configuration.holdBrowserOpen = true;
         }
         @Test
         void fillFormTest() {
@@ -28,15 +31,39 @@ public class TestPracticeForm {
         $("#userEmail").setValue("simple@test.ru");
         $("#userNumber").setValue("8381238899");
         $("#currentAddress").setValue("Lenina Street 2");
-        $(byId("dateOfBirthInput")).click();
+
+        //Радио-баттон (выбор гендера)
+        $("#gender-radio-3").parent().click(); // хороший вариант. Здесь мы обращаемся к родительскому элементу div.
+        $(byText("Male")).click(); // Здесь по тексту мы ищем первый элемент в html. Валидный вариант, если сайт проверяем на одном языке. Если сайт проверяем на нескольких языках, то не очень хороший вариант.
+        $("label[for='gender-radio-1']").click(); // сложный вариант. Используется тогда, когда элементы накладываются друг на друга
+        $("genterWrapper").$(byText("Male")).click(); // самый лучший вариант локатора
+
+        //Календарь
+        $(byId("dateOfBirthInput")).click(); // Кликаем на поле с календарем. Первый вариант.
+        $("dateOfBirthInput").click(); // Кликаем на поле с календарем. Другой вариант.
         $(".react-datepicker__month-container").shouldBe(Condition.visible);
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1991");
         $(byText("14")).click();
-        $("label[for='gender-radio-1']").click();
-        $("#subjectsInput").shouldBe(visible).setValue("English").pressEnter();
-        $("label[for='hobbies-checkbox-1']").click();
-        $("#uploadPicture").uploadFromClasspath("Блейд.jpg "); // Здесь также обращаемся к id="uploadPicture".
+
+        // Автозаполняемое поле
+        $("#subjectsInput").setValue("English").pressEnter();
+
+        //Чек-бокс (выбор хобби)
+        $("hobbiesWrapper").$(byText("Music")).click(); // самый лучший вариант локатора
+        $("label[for='hobbies-checkbox-1']").click(); // сложный вариант. Используется тогда, когда элементы накладываются друг на друга
+
+        //Выбор файла (загрузка картинки)
+        $("#uploadPicture").uploadFromClasspath("Блейд.jpg"); // Самый классный вариант. Здесь также обращаемся к id="uploadPicture".
+        $("#uploadPicture").uploadFile(new File("Блейд.jpg")); // Вариант посложнее. Здесь также обращаемся к id="uploadPicture".
+
+        //Выбор из выпадающего меню
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+
+        //Выбор из выпадающего меню
         $("#state").scrollIntoView(true).shouldBe(visible).click();
         $("#state input").setValue("Haryana").pressEnter();
         $("#city").scrollIntoView(true).shouldBe(visible).click();
