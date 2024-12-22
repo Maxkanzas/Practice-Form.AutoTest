aec72b946352d236306c7c76d30048f9a2cea1f6
maxkanzas <maxkanz1991@gmail.com>
2024-12-22 17:09:35 +0400

    add simpleTest change FinalRegistrationPageTest, RandomData

--- src/test/java/Utils/RandomData.java
+++ src/test/java/Utils/RandomData.java
@@ -2,23 +2,37 @@ package Utils;
 
 import com.github.javafaker.Faker;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Locale;
 
 public class RandomData {
     Faker faker = new Faker(new Locale("en-GB"));
 
-    public String firstName = faker.name().firstName();
+    LocalDate birthDate = faker.date()
+            .birthday(18, 80)
+            .toInstant()
+            .atZone(ZoneId.systemDefault())
+            .toLocalDate();
+
+    // Извлечение дня, месяца и года
+    public String dayOfBirth = String.valueOf(birthDate.getDayOfMonth());
+    public String monthOfBirth = birthDate.getMonth().toString().substring(0, 1) +
+            birthDate.getMonth().toString().substring(1).toLowerCase(); // Название месяца на английском
+    public String yearOfBirth = String.valueOf(birthDate.getYear());
+
+    public String getfirstName(){
+        return faker.name().firstName();
+    }
     public String lastName = faker.name().lastName();
     public String userEmail = faker.internet().emailAddress();
     public String phoneNumber = faker.number().digits(10);
     public String address = faker.address().streetAddress();
     public String gender = faker.options().option("Male", "Female", "Other");
-    public String dayOfBirth = Integer.toString(faker.number().numberBetween(1, 27));
-    public String yearOfBirth = Integer.toString(faker.number().numberBetween(1990, 2023));
-    public String monthOfBirth = faker.options().option("January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December");
+//    public String dayOfBirth = Integer.toString(faker.number().numberBetween(1, 27));
+//    public String yearOfBirth = Integer.toString(faker.number().numberBetween(1990, 2023));
+//    public String monthOfBirth = faker.options().option("January", "February", "March", "April", "May", "June",
+//            "July", "August", "September", "October", "November", "December");
     public String subjects = faker.options().option("History", "Arts", "Maths", "Social Studies");
     public String pictureName = "Блейд.jpg";
     public String hobbies = faker.options().option("Sports", "Reading", "Music");
